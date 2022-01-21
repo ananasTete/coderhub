d759face53b321ebfdc7b6a04fa8a3b9ff015f71
李子航 <2045268468@qq.com>
2022-01-21 15:54:54 +0800

    获取单张flower配图接口

--- src/controller/flower.controller.js
+++ src/controller/flower.controller.js
@@ -1,11 +1,25 @@
-const FlowerService = require('../service/flower.service')
+const fs = require("fs");
+const { FLOWER_IMG_PATH } = require("../constants/file-path");
+const FlowerService = require("../service/flower.service");
 
 class FlowerController {
   async addFlower(ctx, next) {
     const flowerInfo = ctx.request.body;
-    const result = await FlowerService.addFlower(flowerInfo)
-    ctx.response.body = result
+    const result = await FlowerService.addFlower(flowerInfo);
+    ctx.response.body = result;
+  }
+
+  // 获取单张flower配图
+  async getFlowerImg(ctx, next) {
+    try {
+      let { filename, mimetype } = ctx.request.params;
+      mimetype = mimetype.replace("2", "/");
+      ctx.response.set("content-type", mimetype);
+      ctx.response.body = fs.createReadStream(FLOWER_IMG_PATH + "/" + filename);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
-module.exports = new FlowerController()
+module.exports = new FlowerController();
--- src/router/flower.router.js
+++ src/router/flower.router.js
@@ -1,10 +1,13 @@
 const Router = require("koa-router");
 const { verifyAuth } = require("../middleware/auth.middleware");
-const { addFlower } = require("../controller/flower.controller");
+const { addFlower, getFlowerImg } = require("../controller/flower.controller");
 
 const router = new Router({ prefix: "/flower" });
 
 // 添加flower
 router.post("/", verifyAuth, addFlower);
+
+// 获取flower单张配图
+router.post('/img/:filename/:mimetype', getFlowerImg)
 
 module.exports = router;
